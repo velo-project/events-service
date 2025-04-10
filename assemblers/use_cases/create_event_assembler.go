f74202783d700e44a8cc2f0e205dab775a0bf11a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-10 10:26:15 -0300

    feat: connection to db with gorm

--- assemblers/use_cases/create_event_assembler.go
+++ assemblers/use_cases/create_event_assembler.go
@@ -3,9 +3,10 @@ package usecases
 import (
 	"github.com/velo-project/events-service/controllers"
 	"github.com/velo-project/events-service/core/use_cases/create_event/implementation"
+	"gorm.io/gorm"
 )
 
-func CreateEventController() controllers.CreateEventController {
+func CreateEventController(db *gorm.DB) controllers.CreateEventController {
 	usecase := implementation.NewCreateEventUseCase()
 	controller := controllers.NewCreateEventController(&usecase)
 
--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	usecases "github.com/velo-project/events-service/assemblers/use_cases"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	// Connection to Db
+	// TODO: Implement Env Variables to connect to the database
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"localhost", "postgres", "1234", "velo_project", "5432",
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Initializing Api Server
 	router := gin.Default()
 
-	createEventController := usecases.CreateEventController()
+	createEventController := usecases.CreateEventController(db)
 
 	router.POST("/api/v1/event", createEventController.Perform)
 
