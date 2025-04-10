5250857a7be62982f86ce1a1947102c9a05749d2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-10 10:57:44 -0300

    fix: return a new instance instead of a pointer

--- adapters/dao/tables/mappers/event_table_mapper.go
+++ adapters/dao/tables/mappers/event_table_mapper.go
@@ -7,8 +7,8 @@ import (
 	"github.com/velo-project/events-service/core/entities"
 )
 
-func EventToDomainEntity(eventTable *tables.EventTable) *entities.EventEntity {
-	return &entities.EventEntity{
+func EventToDomainEntity(eventTable *tables.EventTable) entities.EventEntity {
+	return entities.EventEntity{
 		ID:          int(eventTable.ID),
 		Name:        eventTable.Name,
 		Description: eventTable.Description.String,
@@ -16,8 +16,8 @@ func EventToDomainEntity(eventTable *tables.EventTable) *entities.EventEntity {
 	}
 }
 
-func EventToPersistanceTable(eventEntity *entities.EventEntity) *tables.EventTable {
-	return &tables.EventTable{
+func EventToPersistanceTable(eventEntity *entities.EventEntity) tables.EventTable {
+	return tables.EventTable{
 		ID:          uint(eventEntity.ID),
 		Name:        eventEntity.Name,
 		Description: sql.NullString{String: eventEntity.Description, Valid: eventEntity.Description != ""},
