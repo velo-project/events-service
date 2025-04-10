99c80028c3384c15dd8600ea77e09974e57fb5da
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-09 22:09:48 -0300

    chore: including routes on main

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	usecases "github.com/velo-project/events-service/assemblers/use_cases"
 )
 
 func main() {
 	router := gin.Default()
+
+	createEventController := usecases.CreateEventController()
+
+	router.POST("/api/v1/event", createEventController.Perform)
 
 	router.Run(":8080")
 }
