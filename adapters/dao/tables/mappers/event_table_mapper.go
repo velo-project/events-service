0ef49d0a11c3d02e5b79cadae0f79278a452b1d2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-10 13:44:59 -0300

    fix: reciving not a pointer of an entity

--- adapters/dao/tables/mappers/event_table_mapper.go
+++ adapters/dao/tables/mappers/event_table_mapper.go
@@ -7,7 +7,7 @@ import (
 	"github.com/velo-project/events-service/core/entities"
 )
 
-func EventToDomainEntity(eventTable *tables.EventTable) entities.EventEntity {
+func EventToDomainEntity(eventTable tables.EventTable) entities.EventEntity {
 	return entities.EventEntity{
 		ID:          int(eventTable.ID),
 		Name:        eventTable.Name,
@@ -16,7 +16,7 @@ func EventToDomainEntity(eventTable *tables.EventTable) entities.EventEntity {
 	}
 }
 
-func EventToPersistanceTable(eventEntity *entities.EventEntity) tables.EventTable {
+func EventToPersistanceTable(eventEntity entities.EventEntity) tables.EventTable {
 	return tables.EventTable{
 		ID:          uint(eventEntity.ID),
 		Name:        eventEntity.Name,
