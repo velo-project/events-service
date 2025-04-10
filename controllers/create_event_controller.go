298498abbda82d555ab98cf1fca26954af79a853
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-10 10:48:50 -0300

    chore: validating input

--- controllers/create_event_controller.go
+++ controllers/create_event_controller.go
@@ -29,6 +29,15 @@ func (cec *CreateEventController) Perform(ctx *gin.Context) {
 		return
 	}
 
+	if errs := input.Validate(); errs != nil {
+		ctx.JSON(400, gin.H{
+			"message":    "Invalid input",
+			"errors":     errs,
+			"statusCode": 400,
+		})
+		return
+	}
+
 	result := cec.usecase.Execute(input)
 
 	ctx.JSON(result.StatusCode, result)
--- core/use_cases/create_event/io/create_event_input.go
+++ core/use_cases/create_event/io/create_event_input.go
@@ -4,3 +4,20 @@ type CreateEventInput struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
+
+func (cei *CreateEventInput) Validate() []string {
+	errors := []string{}
+
+	if cei.Name == "" {
+		errors = append(errors, "Name is required")
+	}
+	if cei.Description == "" {
+		errors = append(errors, "Description is required")
+	}
+
+	if len(errors) > 0 {
+		return errors
+	}
+
+	return nil
+}
