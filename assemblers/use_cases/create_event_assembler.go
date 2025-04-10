f2a2b3acefb95885edfb856a9409c404c768df69
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-10 13:31:06 -0300

    refactor: package on usecases and assemblers

--- assemblers/use_cases/create_event_assembler.go
+++ assemblers/use_cases/create_event_assembler.go
@@ -1,14 +1,16 @@
 package usecases
 
 import (
+	repositoryImplementation "github.com/velo-project/events-service/adapters/dao/repositories/implementation"
 	"github.com/velo-project/events-service/controllers"
-	"github.com/velo-project/events-service/core/use_cases/create_event/implementation"
+	usecaseImplementation "github.com/velo-project/events-service/core/use_cases/create_event/implementation"
 	"gorm.io/gorm"
 )
 
 func CreateEventController(db *gorm.DB) controllers.CreateEventController {
-	usecase := implementation.NewCreateEventUseCase()
-	controller := controllers.NewCreateEventController(&usecase)
+	repository := repositoryImplementation.NewEventRepository(db)
+	usecase := usecaseImplementation.NewCreateEventUseCase(repository)
+	controller := controllers.NewCreateEventController(usecase)
 
 	return controller
 }
--- core/use_cases/create_event/implementation/create_event_usecase_implementation.go
+++ core/use_cases/create_event/implementation/create_event_usecase_implementation.go
@@ -1,15 +1,20 @@
-package implementation
+package usecaseImplementation
 
 import (
+	"github.com/velo-project/events-service/adapters/dao/repositories"
 	"github.com/velo-project/events-service/core/entities"
+	createevent "github.com/velo-project/events-service/core/use_cases/create_event"
 	"github.com/velo-project/events-service/core/use_cases/create_event/io"
 )
 
 type CreateEventUseCase struct {
+	repository repositories.EventRepository
 }
 
-func NewCreateEventUseCase() CreateEventUseCase {
-	return CreateEventUseCase{}
+func NewCreateEventUseCase(repository repositories.EventRepository) createevent.CreateEventUseCase {
+	return &CreateEventUseCase{
+		repository: repository,
+	}
 }
 
 func (ceu *CreateEventUseCase) Execute(input io.CreateEventInput) io.CreateEventOutput {
@@ -19,9 +24,11 @@ func (ceu *CreateEventUseCase) Execute(input io.CreateEventInput) io.CreateEvent
 		PhotoPath:   "",
 	}
 
+	savedEntity := ceu.repository.CreateEvent(event)
+
 	return io.CreateEventOutput{
 		Message:    "success",
-		Event:      event,
+		Event:      savedEntity,
 		StatusCode: 201,
 	}
 }
