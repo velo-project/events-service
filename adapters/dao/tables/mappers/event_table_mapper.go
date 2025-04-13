01e342c12b5bb1f4cb1d291865d234a216dfa300
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-13 18:54:28 -0300

    chore: configuring mapper to new properties

--- adapters/dao/tables/mappers/event_table_mapper.go
+++ adapters/dao/tables/mappers/event_table_mapper.go
@@ -5,22 +5,50 @@ import (
 
 	"github.com/velo-project/events-service/adapters/dao/tables"
 	"github.com/velo-project/events-service/core/entities"
+	"github.com/velo-project/events-service/core/entities/enums"
 )
 
 func EventToDomainEntity(eventTable tables.EventTable) entities.EventEntity {
+	convertIntToBool := func(value int) bool {
+		return value == 1
+	}
+
 	return entities.EventEntity{
-		ID:          int(eventTable.ID),
-		Name:        eventTable.Name,
-		Description: eventTable.Description.String,
-		PhotoPath:   eventTable.PhotoPath.String,
+		ID:                   int(eventTable.ID),
+		Name:                 eventTable.Name,
+		Description:          eventTable.Description.String,
+		PhotoPath:            eventTable.PhotoPath.String,
+		IsActive:             convertIntToBool(eventTable.IsActive),
+		IsCanceled:           convertIntToBool(eventTable.IsCanceled),
+		Address:              eventTable.Address,
+		DifficultyOfTheTrack: enums.DifficultyEnum(eventTable.DifficultyOfTheTrack),
+		TrackType:            enums.TrackTypeEnum(eventTable.TrackType),
+		WhenWillHappen:       eventTable.WhenWillHappen,
+		CreatedAt:            eventTable.CreatedAt,
+		UpdatedAt:            eventTable.UpdatedAt,
 	}
 }
 
 func EventToPersistanceTable(eventEntity entities.EventEntity) tables.EventTable {
+	convertBoolToInt := func(value bool) int {
+		if value {
+			return 1
+		}
+		return 0
+	}
+
 	return tables.EventTable{
-		ID:          uint(eventEntity.ID),
-		Name:        eventEntity.Name,
-		Description: sql.NullString{String: eventEntity.Description, Valid: eventEntity.Description != ""},
-		PhotoPath:   sql.NullString{String: eventEntity.PhotoPath, Valid: eventEntity.PhotoPath != ""},
+		ID:                   uint(eventEntity.ID),
+		Name:                 eventEntity.Name,
+		Description:          sql.NullString{String: eventEntity.Description, Valid: eventEntity.Description != ""},
+		PhotoPath:            sql.NullString{String: eventEntity.PhotoPath, Valid: eventEntity.PhotoPath != ""},
+		IsActive:             convertBoolToInt(eventEntity.IsActive),
+		IsCanceled:           convertBoolToInt(eventEntity.IsCanceled),
+		Address:              eventEntity.Address,
+		DifficultyOfTheTrack: int(eventEntity.DifficultyOfTheTrack),
+		TrackType:            int(eventEntity.TrackType),
+		WhenWillHappen:       eventEntity.WhenWillHappen,
+		CreatedAt:            eventEntity.CreatedAt,
+		UpdatedAt:            eventEntity.UpdatedAt,
 	}
 }
