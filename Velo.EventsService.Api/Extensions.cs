786d8dc1303ee456dbcff60db1290487bc32cfdd
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-24 21:26:44 -0300

    ci: adding persistance to asp.net pipeline

--- Velo.EventsService.Api/Extensions.cs
+++ Velo.EventsService.Api/Extensions.cs
@@ -1,8 +1,10 @@
 using Velo.EventsService.Commands.CreateEvent;
 using Velo.EventsService.Commands.CreateEvent.Handler;
+using Velo.EventsService.Core.Contracts;
 using Velo.EventsService.Core.Dependencies.Dispatchers;
 using Velo.EventsService.Core.Dependencies.Dispatchers.Implementations;
 using Velo.EventsService.Core.Dependencies.Handlers;
+using Velo.EventsService.Persistence.Repositories.Transactional;
 
 namespace Velo.EventsService.Api;
 
@@ -17,5 +19,10 @@ public static class Extensions
     public static void AddCommands(this IServiceCollection services)
     {
         services.AddScoped<ICommandHandler<CreateEventCommand, CreateEventCommandResult>, CreateEventCommandHandler>();
+    }
+
+    public static void AddPersistence(this IServiceCollection services)
+    {
+        services.AddScoped<ITransactionalEventsRepository, TransactionalEventsRepository>();
     }
 }
