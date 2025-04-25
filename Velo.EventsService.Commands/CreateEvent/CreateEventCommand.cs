4c6040fd23217a27d4c63de56a9bb60412ee61a7
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-24 22:16:18 -0300

    chore: validation for command

--- Velo.EventsService.Commands/CreateEvent/CreateEventCommand.cs
+++ Velo.EventsService.Commands/CreateEvent/CreateEventCommand.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 using Velo.EventsService.Core.Dependencies.Contracts;
 
 namespace Velo.EventsService.Commands.CreateEvent;
 
 public class CreateEventCommand : ICommand
 {
+    [Required]
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
+    
+    [Required]
     public DateTime WhenWillHappen { get; set; }
 }
