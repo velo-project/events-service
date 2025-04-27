6e0e537fd46741b61345b45d14fa4cbc75a22178
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-26 21:32:11 -0300

    chore: logger on dispatchers

--- Velo.EventsService.Dependencies/Mediator/Dispatchers/Implementations/CommandDispatcher.cs
+++ Velo.EventsService.Dependencies/Mediator/Dispatchers/Implementations/CommandDispatcher.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Velo.EventsService.Dependencies.Mediator.Contracts;
 using Velo.EventsService.Dependencies.Mediator.Handlers;
 
@@ -6,9 +7,38 @@ namespace Velo.EventsService.Dependencies.Mediator.Dispatchers.Implementations;
 
 public class CommandDispatcher(IServiceProvider serviceProvider) : ICommandDispatcher
 {
-    public Task<TCommandResult> Dispatch<TCommand, TCommandResult>(TCommand command, CancellationToken cancellationToken) where TCommand : ICommand
+    public async Task<TCommandResult> Dispatch<TCommand, TCommandResult>(TCommand command, CancellationToken cancellationToken) where TCommand : ICommand
     {
+        var logger = serviceProvider.GetRequiredService<ILogger<TCommand>>();
         var handler = serviceProvider.GetRequiredService<ICommandHandler<TCommand, TCommandResult>>();
-        return handler.Handle(command, cancellationToken);
+
+        if (handler == null)
+        {
+            var message = $"No Handler Found for <{typeof(TCommand).Namespace}, {typeof(TCommandResult).Namespace}>";
+            logger.LogError(message);
+            throw new Exception(message);
+        }
+
+        try
+        {
+            logger.LogInformation("Dispatching command: {CommandName}", typeof(TCommand).Name);
+            var result = await handler.Handle(command, cancellationToken);
+            logger.LogInformation("Dispatch success: {CommandName}", typeof(TCommand).Name);
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Dispatch failed for {CommandName}", typeof(TCommand).Name);
+            logger.LogError("Exception message: {Message}", ex.Message);
+            logger.LogError("Stack trace: {StackTrace}", ex.StackTrace);
+
+            if (ex.InnerException != null)
+            {
+                logger.LogError("Inner exception message: {InnerException}", ex.InnerException.Message);
+            }
+            
+            throw;
+        }
     }
 }
--- Velo.EventsService.Dependencies/Mediator/Dispatchers/Implementations/QueryDispatcher.cs
+++ Velo.EventsService.Dependencies/Mediator/Dispatchers/Implementations/QueryDispatcher.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Velo.EventsService.Dependencies.Mediator.Contracts;
 using Velo.EventsService.Dependencies.Mediator.Handlers;
 
@@ -6,9 +7,38 @@ namespace Velo.EventsService.Dependencies.Mediator.Dispatchers.Implementations;
 
 public class QueryDispatcher(IServiceProvider serviceProvider) : IQueryDispatcher
 {
-    public Task<TQueryResult> Dispatch<TQuery, TQueryResult>(TQuery query, CancellationToken cancellationToken) where TQuery : IQuery
+    public async Task<TQueryResult> Dispatch<TQuery, TQueryResult>(TQuery query, CancellationToken cancellationToken) where TQuery : IQuery
     {
+        var logger = serviceProvider.GetRequiredService<ILogger<TQuery>>();
         var handler = serviceProvider.GetRequiredService<IQueryHandler<TQuery, TQueryResult>>();
-        return handler.Handle(query, cancellationToken);
+
+        if (handler == null)
+        {
+            var message = $"No Handler Found for <{typeof(TQuery).Namespace}, {typeof(TQueryResult).Namespace}>";
+            logger.LogError(message);
+            throw new Exception(message);
+        }
+
+        try
+        {
+            logger.LogInformation("Dispatching query: {QueryName}", typeof(TQuery).Name);
+            var result = await handler.Handle(query, cancellationToken);
+            logger.LogInformation("Dispatch success: {QueryName}", typeof(TQuery).Name);
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Dispatch failed for {QueryName}", typeof(TQuery).Name);
+            logger.LogError("Exception message: {Message}", ex.Message);
+            logger.LogError("Stack trace: {StackTrace}", ex.StackTrace);
+
+            if (ex.InnerException != null)
+            {
+                logger.LogError("Inner exception message: {InnerException}", ex.InnerException.Message);
+            }
+
+            throw;
+        }
     }
 }
