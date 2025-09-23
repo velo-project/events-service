0f6a2a49b75d631de808445c9da8dea845a3ca97
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-09-22 23:25:59 -0300

    ci: init file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("WARN: No .env file, using default system variables")
+	}
 
+	postgresConn := os.Getenv("POSTGRES_CONNECTION_STRING")
+
+	db, err := sql.Open("postgres", postgresConn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	grpcStr := os.Getenv("USER_SERVICE_GRPC_ADDRESS")
+	grpcConn, err := grpc.NewClient(grpcStr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Can't connect to User Service gRPC Server: %v", err)
+	}
+	defer grpcConn.Close()
+
+	r := gin.Default()
+
+	// pr := r.Group("/api/events/v1", http.AuthMiddleware())
+
+	r.Run()
 }
