4fb824a44748d9c1049e8ae859eb89b1b74e3705
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-09-29 20:53:54 -0300

    feat(cancel_event): add user existence check

--- internal/core/services/cancel_event_service.go
+++ internal/core/services/cancel_event_service.go
@@ -10,15 +10,17 @@ type CancelEventService interface {
 }
 
 type cancelEventService struct {
-	repo ports.CancelEventPort
+	repo               ports.CancelEventPort
+	userExistsByIdPort ports.UserExistsByIdPort
 }
 
-func NewCancelEventService(repo ports.CancelEventPort) CancelEventService {
-	return &cancelEventService{repo: repo}
+func NewCancelEventService(repo ports.CancelEventPort, userExistsByIdPort ports.UserExistsByIdPort) CancelEventService {
+	return &cancelEventService{repo: repo, userExistsByIdPort: userExistsByIdPort}
 }
 
 type CancelEventServiceInput struct {
 	EventId int
+	UserId  int
 }
 
 type CancelEventServiceOutput struct {
@@ -27,6 +29,20 @@ type CancelEventServiceOutput struct {
 }
 
 func (s cancelEventService) Execute(input *CancelEventServiceInput) *CancelEventServiceOutput {
+	exists, err := s.userExistsByIdPort.Execute(input.UserId)
+	if err != nil {
+		return &CancelEventServiceOutput{
+			Message:    "Estamos enfrentando problemas no momento. Tente novamento mais tarde",
+			StatusCode: 502,
+		}
+	}
+	if !exists {
+		return &CancelEventServiceOutput{
+			Message:    "Este usuário não existe",
+			StatusCode: 404,
+		}
+	}
+
 	event, err := s.repo.GetEventById(input.EventId)
 	if err != nil {
 		return &CancelEventServiceOutput{
