84bc947de11995b219e9e3e62ccd3891ae8947ef
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-09-29 20:49:47 -0300

    refactor(errors): improve cancel subscription error handling

--- internal/core/errors/cancel_subscription_errors.go
+++ internal/core/errors/cancel_subscription_errors.go
@@ -5,5 +5,4 @@ import "errors"
 var (
 	ErrBlockedCancelSubscription = errors.New("Não é possível cancelar a inscrição em um evento que acontecerá em 7 dias ou menos")
 	ErrUserSubscriptionNotFound  = errors.New("Inscrição não encontrada para este evento e usuário")
-	ErrEventNotFound             = errors.New("Esse evento não existe")
 )
